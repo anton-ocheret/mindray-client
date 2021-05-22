9de8a2016f9dc3e602dff8390bf98f0fbfdfd143
Anton Ocheret <ao.front.end@gmail.com>
2021-05-22 12:15:21 +0300

    1200367535784598-create-header

--- src/modules/quiz/views/quiz/component.vue
+++ src/modules/quiz/views/quiz/component.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Awesome quiz!</h1>
+  <div></div>
 </template>
 
 <script>
---
+++ src/shared/components/global/the-header/component.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="app-header">
+    <router-link to="/" class="logo">
+      <img :src="require('@images/icons/logo.svg')" />
+    </router-link>
+
+    <div class="app-header__center">
+      <span class="app-text app-text--xs">
+        Техподдержка
+      </span>
+
+      <a href="tel:+7 (499) 495-48-21" class="app-text app-text--lg">
+        +7 (499) 495-48-21
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'the-header',
+};
+</script>
+
+<style lang="scss">
+  .app-header {
+    position: fixed;
+    width: 100%;
+    left: 0;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 30px;
+
+    @media(min-width: $sm) {
+      padding: 30px 35px;
+    }
+  }
+
+  .app-header__center {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    @media(min-width: $sm) {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      align-items: center;
+    }
+  }
+
+  .logo {
+    width: 40px;
+
+    @media(min-width: $sm) {
+      width: auto;
+    }
+  }
+</style>
